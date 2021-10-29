500cc21e83949480294d6b7f7dfb1b481dea0fd4
rajeshtutta143 <93185932+rajeshtutta143@users.noreply.github.com>
2021-10-29 00:11:42 -0700

    Add files via upload

---
+++ Rajesh.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+    // variable declaration  
+    int age;
+	float cgpa;
+	char initial;
+	
+	// initialization
+	age     = 21;
+	cgpa    = 9.0;
+	initial = 'T';
+	//output
+	printf("Age of the student is %d\n", age);
+	printf("cgpa of the student is %f\n", cgpa);
+	printf("initial of the student is %c\n", initial);
+	
+	
+	
+
+}
