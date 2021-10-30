5e28ab178b4b1730bb2401d69f12b784505c1875
rajeshtutta143 <93185932+rajeshtutta143@users.noreply.github.com>
2021-10-29 23:19:59 -0700

    Add files via upload

---
+++ RAJESH 123.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+    //variables
+    int shankar, rajesh, sum;
+
+    
+    //initialization
+    shankar = 156;
+    rajesh  = 143;
+    
+    //calculaion
+    sum = shankar+rajesh;
+
+    //output
+    printf("%d", sum);
+}
