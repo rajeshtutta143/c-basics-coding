a8681e4473c95b359154d9aafa7804662d00e4f1
rajeshtutta143 <93185932+rajeshtutta143@users.noreply.github.com>
2021-11-06 00:02:18 -0700

    Add files via upload

---
+++ rajesh 6-11-2021.c
@@ -0,0 +1,17 @@
+//printing all Arithmatic operations.
+#include<stdio.h>
+void main()
+{
+	int a,b,p,q,r,s,t;
+	a=10,b=2;
+	p=a-b;
+	q=a*b;
+	r=a/b;
+	s=a%b;
+	t=a+b;
+	printf("difference of %d and %d is %d",a,b,p);
+	printf("\nproduct of %d and %d is %d",a,b,q);
+	printf("\ndivision of %d and %d is %d",a,b,r);
+	printf("\nReminder of %d and %d is %d",a,b,s);
+	printf("\nsum of %d and %d is %d",a,b,t);
+}
