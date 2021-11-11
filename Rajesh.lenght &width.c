6d6349d95f9604fe1ec12a8901cf8fef6c7f101a
rajeshtutta143 <93185932+rajeshtutta143@users.noreply.github.com>
2021-11-10 22:56:24 -0800

    Add files via upload

---
+++ Rajesh.lenght &width.c
@@ -0,0 +1,11 @@
+// To find the square of a number
+#include<stdio.h>
+void main()
+{
+	int length,width;
+	printf("enter a length & width");
+	scanf("%d%d",&length,&width);
+	printf("area of the rectangle is %d\n",length*width);
+	printf("perimeter of the rectangle is %d",2*(length+width));
+	
+}
