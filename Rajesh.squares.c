3fe62f6b457c8b4acf43e6a13b6dfe6995054bd3
rajeshtutta143 <93185932+rajeshtutta143@users.noreply.github.com>
2021-11-10 22:42:29 -0800

    Add files via upload

---
+++ Rajesh.squares.c
@@ -0,0 +1,13 @@
+// To find the square of a number
+#include<stdio.h>
+void main()
+{
+	int side,area,perimeter;
+	printf("enter side");
+	scanf("%d",&side);
+	area=side*side;
+	perimeter=4*side;
+	printf("area of the square is %d\n",area);
+	printf("perimeter of the square is %d",perimeter);
+	
+}
